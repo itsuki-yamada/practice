f57542f2e1ddd6148e1400e8e4745300558bcc31
Itsuki Yamada <yamada351035@gmail.com>
2021-04-02 10:52:39 +0900

    [modify] todoを一つ書いてみる

---
+++ 202104WEBDBPRESS-Vue/vue-todo-list/src/components/ToDoList.vue
@@ -0,0 +1,12 @@
+<template>
+    <ul>
+        <li>{{ todo }}</li>
+    </ul>
+</template>
+<script>
+    export default {
+        data() {
+            return { todo: 'Go out to sea' }
+        }
+    }
+</script>
