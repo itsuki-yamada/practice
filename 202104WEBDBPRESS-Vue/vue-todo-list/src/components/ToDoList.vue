c40db97645ac76b98c88f1161872f8525c658af5
Itsuki Yamada <yamada351035@gmail.com>
2021-04-02 11:55:00 +0900

    [modify] 入力欄の作成

--- 202104WEBDBPRESS-Vue/vue-todo-list/src/components/ToDoList.vue
+++ 202104WEBDBPRESS-Vue/vue-todo-list/src/components/ToDoList.vue
@@ -1,4 +1,8 @@
 <template>
+    <!-- 双方向データバインディング　inputの入力内容をプロパティに受け取る -->
+    <input v-model="inputValue">
+    入力した値: {{ inputValue }}
+
     <ul>
         <li v-for="todo in todoItems"
         v-bind:key="todo.id"
@@ -11,6 +15,7 @@
     export default {
         data(){
             return{
+                inputValue: '',
                 todoItems: [
                     {id: 1, text: 'Go out to sea'},
                     {id: 2, text: 'Invite the first member'}
