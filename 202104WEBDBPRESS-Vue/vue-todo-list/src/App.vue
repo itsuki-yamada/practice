9acaa814f15f007e770f2af8147cf38097f5eb18
Itsuki Yamada <yamada351035@gmail.com>
2021-04-05 11:29:08 +0900

    [add] アニメーションの作成

--- 202104WEBDBPRESS-Vue/vue-todo-list/src/App.vue
+++ 202104WEBDBPRESS-Vue/vue-todo-list/src/App.vue
@@ -3,17 +3,22 @@
 
   <!-- ToDolistを使用する -->
   <ToDoList />
+
+  <!-- transitionを使用したアニメーション -->
+  <TextAnimation />
 </template>
 
 <script>
 // ToDoListをインポート
 import ToDoList from './components/ToDoList.vue'
+import TextAnimation from './components/TextAnimation.vue'
 
 export default {
   name: 'App',
   components: {
     // ToDoListを<template>で使用できるようにする
-    ToDoList
+    ToDoList,
+    TextAnimation
   }
 }
 </script>
