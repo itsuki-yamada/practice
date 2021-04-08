a8c2256b11e363cc7513333160e37ec0b8b4ca37
Itsuki Yamada <yamada351035@gmail.com>
2021-04-08 11:45:32 +0900

    [modify] 入力欄に入力された値をToDoリストに追加する処理を追加

--- 20210408_JS_TodoList/todoapp/src/App.js
+++ 20210408_JS_TodoList/todoapp/src/App.js
@@ -1,4 +1,4 @@
-console.log("App.js loaded");
+import { element } from "./view/html-util.js";
 
 export class App {
   constructor() {
@@ -7,10 +7,27 @@ export class App {
   mount() {
     const formElement = document.getElementById("js-form");
     const inputElement = document.getElementById("js-form-input");
+    const containerElement = document.getElementById("js-todo-list");
+    const todoItemCountElement = document.getElementById("js-todo-count");
+
+    let todoItemCount = 0;
+
     formElement.addEventListener("submit", (event) => {
       //   submitイベント本来の動作を止める
       event.preventDefault();
-      console.log(inputElement.value);
+
+      // 追加するToDoItemの要素を作成する。
+      const todoItemElement = element`<li>${inputElement.value}</li>`;
+
+      // ToDoアイテムをcontainerに追加する
+      containerElement.appendChild(todoItemElement);
+
+      // ToDoアイテム数を+1して、表示されているテキストを更新する
+      todoItemCount += 1;
+			todoItemCountElement.textContent = `ToDoアイテム数: ${todoItemCount}`;
+
+      // 入力欄を空文字にしてリセットする
+			inputElement.value = "";
     });
   }
 }
