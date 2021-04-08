64ced0d0f650c10f58e551a4dba100cba0ec2509
Itsuki Yamada <yamada351035@gmail.com>
2021-04-08 10:43:07 +0900

    [modify] 入力欄に入力された値をコンソールログに出力する
    Appクラスにマウント関数を追加し、フォームエレメントのイベントリスナーを作成。
    preventDefault関数を使用してeventの従来の動作を停止し、ログ出力を行う。

--- 20210408_JS_TodoList/todoapp/src/App.js
+++ 20210408_JS_TodoList/todoapp/src/App.js
@@ -4,4 +4,13 @@ export class App {
   constructor() {
     console.log("App initialized");
   }
+  mount() {
+    const formElement = document.getElementById("js-form");
+    const inputElement = document.getElementById("js-form-input");
+    formElement.addEventListener("submit", (event) => {
+      //   submitイベント本来の動作を止める
+      event.preventDefault();
+      console.log(inputElement.value);
+    });
+  }
 }
