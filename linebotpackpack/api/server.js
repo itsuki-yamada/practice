fb9dd95ad57aa9ac54af272652f8efaa36725964
Itsuki Yamada <yamada351035@gmail.com>
2021-04-19 07:22:33 +0900

    [add] オウム返しbot

---
+++ linebotpackpack/api/server.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const express = require("express");
+const line = require("@line/bot-sdk");
+const PORT = process.env.PORT || 3000;
+
+const config = {
+  channelSecret: "チャンネルキー",
+  channelAccessToken:
+    "シークレットチャンネルトークン",
+};
+
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello LINE BOT!(GET)")); //ブラウザ確認用(無くても問題ない)
+
+app.post("/webhook", line.middleware(config), (req, res) => {
+  console.log(req.body);
+  console.log(req.body.source);
+
+  Promise.all(req.body.events.map(handleEvent)).then((result) =>
+    res.json(result)
+  );
+});
+
+const client = new line.Client(config);
+
+async function handleEvent(event) {
+  if (event.type !== "message" || event.message.type !== "text") {
+    return Promise.resolve(null);
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: "text",
+    text: event.message, //実際に返信の言葉を入れる箇所
+  });
+}
+
+// app.listen(PORT);
+// console.log(`Server running at ${PORT}`);
+
+process.env.NOW_REGION ? (module.exports = app) : app.listen(PORT);
+console.log(`Server running at ${PORT}`);
