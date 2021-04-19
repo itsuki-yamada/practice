8bf02c3ca304d84af5372fb9d784db6b0c8115a3
Itsuki Yamada <yamada351035@gmail.com>
2021-04-19 09:42:19 +0900

    [modify] Bordクラスでデータを管理するように変更

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,13 +4,42 @@ import "./index.css";
 
 class Square extends React.Component {
   render() {
-    return <button className="square">{this.props.value}</button>;
+    return (
+      <button
+        className="square"
+        onClick={() => {
+          this.props.onClick();
+        }}
+      >
+        {this.props.value}
+      </button>
+    );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      suquares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.suquares.slice();
+    squares[i] = "X";
+    this.setState({ squares: squares });
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.suquares[i]}
+        onClick={() => {
+          this.handleClick(i);
+        }}
+      />
+    );
   }
 
   render() {
